2c4528868c5356d698217608c8e4bb317ccf7a64
Steven Bore <smtbore@gmail.com>
2020-07-17 14:19:18 +0100

    New repo for all QA work - Initial commit - Daily challenge 16th July

---
+++ QAC_Daily_Challenges/challenge_16th_July.py
@@ -0,0 +1,17 @@
+firstNames=["Christopher", "John", "Jason", 
+"Arsalan", "Domenico", "Joshua", "Ryan", "Bradley",
+"Javas", "Sithembiso", "Jacob", "Clifford", "Wasim",
+"Mohamed", "Tobias", "Christopher", "Samuel", "Edmund",
+"Amanda", "Steven", "Diarmuid"]
+
+firstNames.append("Luke")
+
+listNumber=str(len(firstNames))
+
+chrisNumber=str(firstNames.count("Christopher"))
+
+print("There are "+listNumber+" people in the list")
+
+print("The 5th person in the list is: "+firstNames[4])
+
+print("There are "+chrisNumber+" people named christopher in the cohort")
